f81677767fccbb4481ccfde404a0b53b62d0d6a9
Zenok4 <dungk4.camlo@gmail.com>
2026-01-18 14:38:27 +0700

    init app

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,7 +19,7 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <HomeScreen/>
     </SafeAreaProvider>
   );
 }
