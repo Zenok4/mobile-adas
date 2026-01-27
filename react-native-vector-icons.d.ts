9160ca04490c9fabf841cfbdd36f25a0c030be69
Zenok4 <dungk4.camlo@gmail.com>
2026-01-27 17:15:20 +0700

    Add FontAwesome icons support and update HomeScreen component

---
+++ react-native-vector-icons.d.ts
@@ -0,0 +1,275 @@
+declare module "react-native-vector-icons/FontAwesome" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontAwesomeProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const FontAwesome: React.ComponentType<FontAwesomeProps>;
+  export default FontAwesome;
+}
+
+declare module "react-native-vector-icons/MaterialIcons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface MaterialIconsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const MaterialIcons: React.ComponentType<MaterialIconsProps>;
+  export default MaterialIcons;
+}
+
+declare module "react-native-vector-icons/Ionicons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface IoniconsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Ionicons: React.ComponentType<IoniconsProps>;
+  export default Ionicons;
+}
+
+/* ===================== BỔ SUNG CÁC ICON KHÁC ===================== */
+
+declare module "react-native-vector-icons/MaterialCommunityIcons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface MaterialCommunityIconsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const MaterialCommunityIcons: React.ComponentType<MaterialCommunityIconsProps>;
+  export default MaterialCommunityIcons;
+}
+
+declare module "react-native-vector-icons/Feather" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FeatherProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Feather: React.ComponentType<FeatherProps>;
+  export default Feather;
+}
+
+declare module "react-native-vector-icons/AntDesign" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface AntDesignProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const AntDesign: React.ComponentType<AntDesignProps>;
+  export default AntDesign;
+}
+
+declare module "react-native-vector-icons/Entypo" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface EntypoProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Entypo: React.ComponentType<EntypoProps>;
+  export default Entypo;
+}
+
+declare module "react-native-vector-icons/EvilIcons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface EvilIconsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const EvilIcons: React.ComponentType<EvilIconsProps>;
+  export default EvilIcons;
+}
+
+declare module "react-native-vector-icons/SimpleLineIcons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface SimpleLineIconsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const SimpleLineIcons: React.ComponentType<SimpleLineIconsProps>;
+  export default SimpleLineIcons;
+}
+
+declare module "react-native-vector-icons/Octicons" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface OcticonsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Octicons: React.ComponentType<OcticonsProps>;
+  export default Octicons;
+}
+
+declare module "react-native-vector-icons/Zocial" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface ZocialProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Zocial: React.ComponentType<ZocialProps>;
+  export default Zocial;
+}
+
+declare module "react-native-vector-icons/Foundation" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FoundationProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Foundation: React.ComponentType<FoundationProps>;
+  export default Foundation;
+}
+
+declare module "react-native-vector-icons/FontAwesome5" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontAwesome5Props extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    solid?: boolean;
+    brand?: boolean;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const FontAwesome5: React.ComponentType<FontAwesome5Props>;
+  export default FontAwesome5;
+}
+
+declare module "react-native-vector-icons/FontAwesome5Brands" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontAwesome5BrandsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const FontAwesome5Brands: React.ComponentType<FontAwesome5BrandsProps>;
+  export default FontAwesome5Brands;
+}
+
+declare module "react-native-vector-icons/FontAwesome6" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontAwesome6Props extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    solid?: boolean;
+    brand?: boolean;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const FontAwesome6: React.ComponentType<FontAwesome6Props>;
+  export default FontAwesome6;
+}
+
+declare module "react-native-vector-icons/FontAwesome6Brands" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontAwesome6BrandsProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const FontAwesome6Brands: React.ComponentType<FontAwesome6BrandsProps>;
+  export default FontAwesome6Brands;
+}
+
+declare module "react-native-vector-icons/Fontisto" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface FontistoProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Fontisto: React.ComponentType<FontistoProps>;
+  export default Fontisto;
+}
+
+declare module "react-native-vector-icons/Lucide" {
+  import * as React from "react";
+  import { TextProps, StyleProp, TextStyle } from "react-native";
+
+  export interface LucideProps extends TextProps {
+    name: string;
+    size?: number;
+    color?: string;
+    style?: StyleProp<TextStyle>;
+  }
+
+  const Lucide: React.ComponentType<LucideProps>;
+  export default Lucide;
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
-import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { useState } from "react";
+import { View } from "react-native";
+import { Button } from "react-native-paper";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 const HomeScreen = () => {
-    const [text, setText] = useState("Welcome to the Home Screen!")
+  const [text, setText] = useState("Welcome to the Home Screen!");
+
   return (
-    <View className="flex flex-1 justify-center items-center">
-      <Text className="font-bold text-2xl text-red-500">
-        <Button mode="contained-tonal" onPress={() => setText("Test")}>
-          {text}
-        </Button>
-      </Text>
+    <View className="flex-1 justify-center items-center">
+      <Button
+        mode="contained-tonal"
+        icon={({ size, color }) => (
+          <FontAwesome5 name="biohazard" size={size} color={color} />
+        )}
+        onPress={() => setText("Test")}
+      >
+        {text}
+      </Button>
     </View>
   );
 };
